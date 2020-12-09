f517a2dc3f4936dbdaafacdf64dd1ff81cc300d9
夏先鹏 <951256916>
2020-12-09 23:09:42 +0800

    Sentinel  sentinel整合feign

--- content-center/src/main/java/com/example/content/feign/client/UserCenterFeignClient.java
+++ content-center/src/main/java/com/example/content/feign/client/UserCenterFeignClient.java
@@ -1,6 +1,7 @@
 package com.example.content.feign.client;
 
 import com.example.common.domain.dto.user.UserDTO;
+import com.example.content.feign.client.fallbackfactory.UserCenterFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @author xianpeng.xia
  * on 2020/12/6 下午11:31
  */
-@FeignClient(name = "user-center")
-    public interface UserCenterFeignClient {
+@FeignClient(name = "user-center",
+    //fallback = UserCenterFeignClientFallback.class,
+    fallbackFactory = UserCenterFeignClientFallbackFactory.class)
+public interface UserCenterFeignClient {
 
     // http://user-center/user/{id}
     @GetMapping("/user/{id}")
