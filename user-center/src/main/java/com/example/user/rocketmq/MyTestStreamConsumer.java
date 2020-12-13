b71bc4ee8a1ebe481e357923620e6bc37c80d41e
夏先鹏 <951256916>
2020-12-13 23:20:07 +0800

    RocketMQ 异常处理

--- user-center/src/main/java/com/example/user/rocketmq/MyTestStreamConsumer.java
+++ user-center/src/main/java/com/example/user/rocketmq/MyTestStreamConsumer.java
@@ -3,6 +3,8 @@ package com.example.user.rocketmq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +18,13 @@ public class MyTestStreamConsumer {
     @StreamListener(MySink.MY_INPUT)
     public void receive(String messageBody) {
         log.info("Get my message {}", messageBody);
+        throw new IllegalArgumentException();
     }
+
+    @StreamListener("errorChannel")
+    public void error(Message<?> message) {
+        ErrorMessage errorMessage = (ErrorMessage) message;
+        log.info("Error {}", errorMessage);
+    }
+
 }
