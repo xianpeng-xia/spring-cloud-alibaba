4d9832e4bea9e7e9cd78cb3042083d271dd0d847
夏先鹏 <951256916>
2020-12-15 23:28:12 +0800

    Gateway 过滤器

---
+++ gateway/src/main/java/com/example/gateway/filter/PreLogGatewayFilterFactory.java
@@ -0,0 +1,28 @@
+package com.example.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * @author xianpeng.xia
+ * on 2020/12/15 下午10:57
+ */
+@Component
+@Slf4j
+public class PreLogGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return (exchange, chain) -> {
+            log.error("请求进来了 ... {},{}", config.getName(), config.getValue());
+            // 修改请求
+            ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().build();
+            ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+            return chain.filter(modifiedExchange);
+        };
+    }
+}
