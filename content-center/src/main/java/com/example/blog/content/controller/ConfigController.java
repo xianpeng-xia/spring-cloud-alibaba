c36609dc09e95eae897804d4909237fd7cf1c880
夏先鹏 <2825615792@xx.com>
2020-12-19 14:21:44 +0800

    Nacos config :配置管理

---
+++ content-center/src/main/java/com/example/blog/content/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.example.blog.content.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @date 2020/12/19
+ * @time 下午12:40
+ */
+@RestController
+@RequestMapping("/nacos/config")
+@RefreshScope
+public class ConfigController {
+
+    //@Value("${your.configuration}")
+    public String yourConfiguration;
+
+    @GetMapping("/get")
+    public String yourConfiguration() {
+        return yourConfiguration;
+    }
+}
